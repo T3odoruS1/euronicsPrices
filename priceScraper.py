7181cab222f548dbdf4cdbf9b6939bc210a4c1d8
edvildt <edgarvildt@gmail.com>
2022-07-18 23:54:01 +0300

    Code updated. Speed increased. Some errors found and dealt with.

--- priceScraper.py
+++ priceScraper.py
@@ -10,7 +10,8 @@ from sys import argv
 
 # Use this when debugging in IDE
 search_matter = input("Please enter the product you would like to search for -> ") + " "
-search_matter += input("Now specify the type of the product. For example 'notebook, smartphone, tablet, speaker' ect -> ")
+search_matter += input("Now specify the type of the product.\n"
+                       " For example 'notebook, smartphone, tablet, speaker' ect -> ")
 
 delay = 10
 
@@ -26,19 +27,24 @@ def toggle_search_bar_and_search(search_string : str):
     WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "autocomplete__input")))
     input = driver.find_element(By.CLASS_NAME, "autocomplete__input")
     input.send_keys(search_matter)
+    time.sleep(0.5)
     input.send_keys(Keys.RETURN)
 
 def filter_goods_by_brand():
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "responsive-filter-button")))
     driver.find_element(By.CLASS_NAME, "responsive-filter-button").click()
-    time.sleep(2)
+    WebDriverWait(driver, delay).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "sort-menu__button ")))
     data_filters = driver.find_elements(By.CLASS_NAME, "sort-menu__button ")
-    time.sleep(0.5)
     data_filters[0].click()
-    time.sleep(2)
+
 
     # Get sort menu
+    WebDriverWait(driver, delay).\
+        until(EC.presence_of_element_located((By.XPATH, '//*[@id="category-filter-panel"]/div/section/ul/li[1]/ul')))
     sort_menu = driver.find_element(By.XPATH, '//*[@id="category-filter-panel"]/div/section/ul/li[1]/ul')
+
     # Get sort menu submenu
+    WebDriverWait(sort_menu, delay).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'sort-menu__item')))
     results = sort_menu.find_elements(By.CLASS_NAME, 'sort-menu__item')
     assert len(results) > 0
     # Get category names for the user choice
@@ -47,12 +53,13 @@ def filter_goods_by_brand():
         if results[i].text == "":
             continue
         text_results.append(results[i].text.split('\n')[0])
-    print('Giving the user options to choose the brand')
-    print('Choose the brand using the given categories')
+
+    print('Choose the brand using the given categories \n')
     for el2 in text_results:
        print(str(text_results.index(el2) + 1) + " -> " + el2)
     chosen_brand = text_results[int(input("And the number is -> ")) - 1]
     print("You have chose ", chosen_brand)
+    print("\n\n")
     # Click the button with a chosen brand
     for i in range(len(text_results)):
         if text_results[i] == chosen_brand:
@@ -64,14 +71,19 @@ def gather_price_info():
     update = True
     while update:
         try:
+            WebDriverWait(driver, 3). \
+                until(
+                EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div/div[2]/button')))
             driver.find_element(By.XPATH,
                                 '/html/body/div[7]/div/div[2]/button').click()
-            print("pressed")
-            time.sleep(1)
+            time.sleep(0.5)
+            print("New products revealed")
         except Exception:
-            print("no more products to reveal")
+            print("No more products to reveal\n\n")
             update = False
 
+    WebDriverWait(driver, delay).\
+        until(EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div/div[2]/section[3]')))
     product_list = driver.find_element(By.XPATH, '/html/body/div[7]/div/div[2]/section[3]')
 
     articles = []
@@ -96,11 +108,17 @@ def gather_price_info():
 
     prices = []
     for el in articles:
-        if "DISCOUNT" in el.text:
-            price_of_element = el.text.split('\n')[5]
-        else:
-            price_of_element = el.text.split('\n')[3]
-        prices.append(price_of_element)
+        print(el.text)
+        try:
+            if "DISCOUNT" in el.text:
+                price_of_element = el.text.split('\n')[5]
+            elif "€" in el.text.split('\n')[3]:
+                price_of_element = el.text.split('\n')[3]
+            elif "€" in el.text.split('\n')[4]:
+                price_of_element = el.text.split('\n')[4]
+            prices.append(price_of_element)
+        except IndexError:
+            print("One was without the price ;(")
 
     return prices
 
@@ -115,7 +133,10 @@ def avg_price(numbers):
 def analyze_product_prices(prices):
     float_price_list = []
     for price in prices:
-        float_price_list.append(float(price[:-3].replace(" ", ".")))
+        try:
+            float_price_list.append(float(price[:-3].replace(" ", ".")))
+        except ValueError:
+            float_price_list.append(float(price[:-3]))
     minimal_price = min(float_price_list)
     maximal_price = max(float_price_list)
     average_price = avg_price(float_price_list)
@@ -130,14 +151,17 @@ def analyze_product_prices(prices):
     print(average_price)
 
 def main():
-    toggle_search_bar_and_search(search_matter)
-    print("Search successful")
-    filter_goods_by_brand()
-    print("filtering successful")
-    prices = gather_price_info()
-    print("price info gathered successful")
-    analyze_product_prices(prices)
-    driver.quit()
+    try:
+        toggle_search_bar_and_search(search_matter)
+        print("Search successful\n------------")
+        filter_goods_by_brand()
+        print("filtering successful\n------------")
+        prices = gather_price_info()
+        print("price info gathered successful")
+        analyze_product_prices(prices)
+        print("\n\n\n")
+    finally:
+        driver.quit()
 
 if __name__ == '__main__':
     main()
